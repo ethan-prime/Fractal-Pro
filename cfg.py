595bce893695027cdeb1288e2e9704dc804c8710
Ethan <emh419@hotmail.com>
2023-08-27 23:02:42 -0500

    updated cfg

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import math, cmath
 
-INSTALLATION_PATH = r"C:\Users\Ethan\Desktop\code\FractalPro"
+INSTALLATION_PATH = r"C:\Path\To\Installation\FractalPro"
 
 C = complex(-0.835, 0.2321)
 MAX_ITERS = 100
