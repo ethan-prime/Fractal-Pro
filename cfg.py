0ba02415da34d8b53b43f8936309e0cc08eb1c75
Ethan <emh419@hotmail.com>
2023-08-29 21:00:24 -0500

    update cfg so threading not used by default, must be manually enabled

--- cfg.py
+++ cfg.py
@@ -2,8 +2,8 @@ import math, cmath
 
 #SYSTEM VARIABLES 
 INSTALLATION_PATH = r"C:\Path\To\Installation\Fractal-Pro" #where Fractal-Pro is installed. Please make sure to include \FractalPro at the end of the path just like my example.
-THREADS = 2 #number of threads used to generate new fractals. if set to 1, there will be no threading done.
+THREADS = 1 #number of threads used to generate new fractals. if set to 1, there will be no threading done.
 
 #DEFAULT VARIABLES FOR GENERATING FRACTALS. OVERRIDEN BY FORM WHEN GENERATING.
 C = complex(-0.835, 0.2321) #used for generating fractals. see documentation on wikipedia!
-MAX_ITERS = 50 #used for generating fractals. see documentation on wikipedia!
+MAX_ITERS = 100 #used for generating fractals. see documentation on wikipedia!
